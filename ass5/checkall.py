99f1aa496631dd897ee55d6dfc661b638cab252e
vaibhavb1996 <vbansal@bu.edu>
2018-10-24 14:56:48 -0400

    worked on 602 assignment 5

---
+++ ass5/checkall.py
@@ -0,0 +1,82 @@
+""" Check all collision programs, record pass/fail
+
+Using the tester (YOU must finish tester), check
+all executable collision programs for correctness.
+
+This program is provided to you 
+as a service, so that you can focus on writing your
+test suite not have to learn the json and glob modules
+"""
+import unittest
+import importlib
+import glob
+import io
+import sys
+import json
+
+import tester
+
+SUPPRESS_OUTPUT = False
+
+
+def check_all_files():
+    passed, failed = [], []
+
+    Programs = glob.glob('collisionc*')
+    for file_name in Programs:
+        if "hard" in file_name: continue
+
+        loader = unittest.loader.TestLoader()
+        results = unittest.result.TestResult()
+
+        try:
+
+            if SUPPRESS_OUTPUT:
+                s = io.StringIO()
+                sys.stdout = s
+
+            tester.PROGRAM_TO_TEST = file_name
+
+            tests = loader.loadTestsFromTestCase(
+                tester.CollisionTestCase)
+
+            tests.run(results)
+
+            tests_passed = results.testsRun - len(results.failures) - len(
+                results.errors)
+
+            if results.wasSuccessful():
+                passed.append(file_name)
+            else:
+                failed.append(file_name)
+            if SUPPRESS_OUTPUT:
+                sys.stdout = sys.__stdout__
+
+        except Exception as e:
+            if SUPPRESS_OUTPUT:
+                sys.stdout = sys.__stdout__
+
+            print('exception', file_name, e)
+            failed.append(file_name)
+    
+    print('Run {} tests'.format(results.testsRun))
+
+    print('you passed {} tests'.format(tests_passed))
+    for test,output in results.failures:
+        print(">>",test)
+        print(">>",output)
+    for test,output in results.errors:
+        print(">>",test)
+        print(">>",output)
+    return passed, failed
+
+
+if __name__ == "__main__":
+    passed, failed = check_all_files()
+    Results = {
+        'failed': failed,
+        'passed': passed,
+        'authors': tester.AUTHORS
+    }
+    with open('tester_results.json', 'w') as f:
+        json.dump(Results, f, indent=4)
